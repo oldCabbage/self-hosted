97e971244142ae3eb2b18eb2a65fdcafddbcfa75
qinmh <2252624036@qq.com>
2025-09-03 17:06:23 +0800

    fix: stat命令macos系统处理

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -5,7 +5,9 @@
 echo "${_group}Ensuring correct permissions on profiles directory ..."
 
 # Check if the parent directory of /var/vroom/sentry-profiles is already owned by vroom:vroom
-if [ "$(stat -c '%U:%G' /var/vroom)" = "vroom:vroom" ]; then
+# if [ "$(stat -c '%U:%G' /var/vroom)" = "vroom:vroom" ]; then
+# macOS版本
+if [ "$(stat -f "%Su:%Sg" "$PROFILES_DIR")" != "vroom:vroom" ]; then
   echo "Ownership of /var/vroom is already set to vroom:vroom. Skipping chown."
 else
   $dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
